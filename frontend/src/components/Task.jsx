8508e0170faed9186aa412fb0fc8d0eb651f3021
Mehthab <mehthabunstoppable@gmail.com>
2025-07-21 15:03:40 +0530

    a

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,24 +1,22 @@
-import { useState } from 'react'
+import React, { useState } from 'react';
 import '../style/Task.css'
 
 function Task() {
+  const [checked, setChecked] = useState(false);
 
-    return (
-      <>
-        <div className='task-cont'>
-                <div class="newCheckbox">
-                    <input type="checkbox" class="checkbox" />
-                    <div class="newCheckboxColor">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg>
-                    </div>
-                    
-                </div>            <input type="checkbox" />
-
-            <p>do the thing</p>
-        </div>
-
-      </>
-    )
+  return (
+    <label className="checkbox-wrapper">
+      <input
+        type="checkbox"
+        checked={checked}
+        onChange={() => setChecked(!checked)}
+      />
+      <span className={`custom-box ${checked ? "checked" : "unchecked"}`}>
+        {checked && "✔"}
+      </span>
+      <span className="label-text">I agree to the terms</span>
+    </label>
+  );
 }
 
 export default Task
