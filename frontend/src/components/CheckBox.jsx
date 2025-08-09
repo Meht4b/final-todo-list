24893d432f4c153579c2e6686ad452f7186500da
Mehthab <mehthabunstoppable@gmail.com>
2025-08-10 04:43:59 +0530

    a

--- frontend/src/components/CheckBox.jsx
+++ frontend/src/components/CheckBox.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import emptyCheck from '../assets/stop.png'
+import filledCheck from '../assets/tick-square.png'
 
-const CheckBox = ({currentStatus, toggleComplete}) => {
+const CheckBox = ({isCompleted, toggleComplete}) => {
   return (
+    <>
     <div>
-      
+      <img src={emptyCheck} />
+      <img src={filledCheck} alt="filled check" className={isCompleted == true ? 'check-box-visible':'check-box-invisible'} /> 
     </div>
+    </>
   )
 }
 
