b4c7d3f7c44f7e6e6217c18cfe8a52fb079595ed
Mehthab <mehthabunstoppable@gmail.com>
2025-09-03 17:01:24 +0530

    a

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -9,7 +9,7 @@ const Task = ({task }) => {
   return (
     <div className='task-item-cont'>
         <CheckBox isCompleted={task.isCompleted}  id = {task.id} />
-        <p>{task.title}</p>
+        <p>{task.task_desc}</p>
     </div>
   )
 }
